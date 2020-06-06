e2ac613ebed9511f5ac9e38b23996877849e52eb
FFrostYY <faticfilip98@gmail.com>
2020-06-06 18:54:51 +0200

    Update generate_ground_truth.py

--- generate_ground_truth.py
+++ generate_ground_truth.py
@@ -1,3 +1,8 @@
+########################################################
+#This file serves to generate a ground truth binary file
+#for use with the compute_detection_metrics tool
+########################################################
+
 import os
 import tensorflow.compat.v1 as tf
 import numpy as np
@@ -10,27 +15,33 @@ from waymo_open_dataset.utils import frame_utils
 from waymo_open_dataset import dataset_pb2
 from waymo_open_dataset import label_pb2
 from waymo_open_dataset.protos import metrics_pb2
+#opens individual record file
+#for the entire dataset, this should be made to iterate through all files
+dataset = tf.data.TFRecordDataset("REPLACE_WITH_FILEPATH", compression_type='')
 
-dataset = tf.data.TFRecordDataset("/mnt/pool0-100/shape-it/validation_data/segment-967082162553397800_5102_900_5122_900_with_camera_labels.tfrecord", compression_type='')
-ground = open('ground.bin', 'wb')
-objects = metrics_pb2.Objects()
-for data in dataset:
-    print("enter first loop")
-    frame = dataset_pb2.Frame()
-    frame.ParseFromString(bytearray(data.numpy()))
+
+ground = open('ground.bin', 'wb')   #create file to store annotations
+objects = metrics_pb2.Objects()     #create Objects() iterable to store all detected objects
+for data in dataset:                #iterates through all frames in the record file
+    frame = dataset_pb2.Frame()     #initialize frame object
+    frame.ParseFromString(bytearray(data.numpy()))      #frame object must first be parsed to be used
+    
+    #iterates through all camera labels
+    #This refers to the cameras at the front and different sides
     for camera_labels in frame.camera_labels:
-        print("enter second loop")
-        o = metrics_pb2.Object()
-        o.camera_name = camera_labels.name
-        o.context_name = frame.context.name
-        o.frame_timestamp_micros = frame.timestamp_micros
+        o = metrics_pb2.Object()    #initialize Object() to store object annotations
+        o.camera_name = camera_labels.name              #copy camera name into object metadata
+        o.context_name = frame.context.name             #copy name of context into object metadata
+        o.frame_timestamp_micros = frame.timestamp_micros       #copy timestamp of frame into object metadata
+        
+        #iterate through all labeled objects in image
         for label in camera_labels.labels:
-            box = label.box
-            o.object.box.CopyFrom(box)
-            o.object.type = label.type
-            o.object.id = ""
-            o.object.detection_difficulty_level = label.detection_difficulty_level
-            o.object.num_lidar_points_in_box: 6
+            box = label.box         #intialize box format
+            o.object.box.CopyFrom(box)      #copy box information - coordinates and dimensions
+            o.object.type = label.type      #copy type of object - car, bicycle, sign or pedestrian
+            o.object.id = ""                #was unsure how to extract this information
+            o.object.detection_difficulty_level = label.detection_difficulty_level      #difficulty level of object - used when dividing results into difficulty classes on eval server
+            o.object.num_lidar_points_in_box: 6                 #num of lidar points - not used in 2d detection - should still be populated by value >5
         objects.objects.append(o)
     break
 ground.write(objects.SerializeToString())
